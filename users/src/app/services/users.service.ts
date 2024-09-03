0f9112660c0d2a597b0c8d1148140a256c59e08d
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-09-03 23:37:47 +0200

    getALL and getByID methods added in service

--- users/src/app/services/users.service.ts
+++ users/src/app/services/users.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IUser } from '../interfaces/iuser.interface';
+import { first, firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private baseUrl: string = "https://peticiones.online/api/users/"
+  private http = inject(HttpClient);
 
-  constructor() { }
+  /**
+   * GETALL()
+   * return Promise<IUser[]>
+   */
+  getAll(page: number): Promise<IUser[]> {
+    return firstValueFrom(this.http.get<IUser[]>(`${this.baseUrl}?page=${page}`))
+  }
+
+  getByID(id: string): Promise<IUser> {
+    return firstValueFrom(this.http.get<IUser>(`${this.baseUrl}${id}`))
+
+  }
 }
