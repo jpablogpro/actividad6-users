eeba8b7d71c1042479065897ac9f1415f2d908b9
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-09-03 22:32:07 +0200

    routes system defined

--- users/src/app/app.routes.ts
+++ users/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { FormComponent } from './pages/form/form.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: 'full', redirectTo: 'home'},
+    { path: "home", component: UserListComponent},
+    { path: "user/:id", component: UserViewComponent },
+    { path: "newuser", component: FormComponent },
+    { path: "updateuser/:id", component: FormComponent },
+    { path: "**", redirectTo: 'home'}
+];
