ddc4c1cbceda647c4aba268e930b673d755557c5
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-09-08 00:14:20 +0200

    user view component modified

--- users/src/app/pages/user-view/user-view.component.ts
+++ users/src/app/pages/user-view/user-view.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUser } from '../../interfaces/iuser.interface';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
+  user: IUser | null = null;
+  activatedRoute = inject(ActivatedRoute);
+  usersService = inject(UsersService)
 
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(async (params: any) => {
+      let id = params.id;
+      this.user = await this.usersService.getByID(id);
+    })
+  }
 }
