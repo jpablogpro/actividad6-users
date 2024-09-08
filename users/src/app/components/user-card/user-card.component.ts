dacb7e090689236f8e2cbde02c2d189e7e3cc6eb
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-09-08 10:14:04 +0200

    refactoring

--- users/src/app/components/user-card/user-card.component.ts
+++ users/src/app/components/user-card/user-card.component.ts
@@ -5,7 +5,7 @@ import { UsersService } from '../../services/users.service';
 import Swal from 'sweetalert2';
 import { SweetAlertIcon } from 'sweetalert2';
 
-type AlertResponse = { title: string; text: string; icon: SweetAlertIcon };
+type AlertResponse = { title: string; text: string; icon: SweetAlertIcon, cbutton: string};
 
 @Component({
   selector: 'app-user-card',
@@ -19,29 +19,24 @@ export class UserCardComponent {
   usersService = inject(UsersService);
 
   async delete(_id: string | undefined) {
-    let alert_res: AlertResponse;
     let borrado = confirm('Deseas Borrar el Usuario con ID: ' + _id + '?');
     if (borrado) {
+      let alert_res: AlertResponse;
       try {
         const res = await this.usersService.deleteByID(_id);
-        if ('_id' in res && res['_id'] === _id) {
-          Swal.fire({
-            title: 'Perfecto!',
-            text: 'Usuario con ID: ' + _id + ' Borrado con exito',
-            icon: 'success',
-            confirmButtonText: 'Aceptar'
-          })
+        if ('_id' in res && res._id === _id) {
+          alert_res = {title: 'Perfecto!', text: 'Usuario con ID: ' + _id + ' Borrado con exito', icon: 'success', cbutton: 'Aceptar'}
         } else {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Usuario con ID: ' + _id + ' No Encontrado',
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
-          })
+          let text: string;
+          // text = ('error' in res) ?  res.error: 'Usuario con ID: ' + _id + ' Borrado con exito'
+          text = ('error' in res) ?  'Ha ocurrido un error' : 'Usuario con ID: ' + _id + ' No encontrado'
+          alert_res = {title: 'Error!', text: text, icon: 'error', cbutton: 'Aceptar'}
         }
       } catch (error) {
         console.log(error);
+        alert_res = {title: 'Error!', text: 'Ha ocurrido un error', icon: 'error', cbutton: 'Aceptar'}
       }
+      Swal.fire(alert_res)
     }
   }
 }
