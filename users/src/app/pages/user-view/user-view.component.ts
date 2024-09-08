8801e1ce085fd61608d4fd21fc79211cb7f9d621
Jose Pablo Gonzalez <jpablogpro@hotmail.com>
2024-09-08 19:54:27 +0200

    buttons added

--- users/src/app/pages/user-view/user-view.component.ts
+++ users/src/app/pages/user-view/user-view.component.ts
@@ -1,7 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { IUser } from '../../interfaces/iuser.interface';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { UsersService } from '../../services/users.service';
+import Swal from 'sweetalert2';
+import { SweetAlertIcon } from 'sweetalert2';
+
+type AlertResponse = { title: string; text: string; icon: SweetAlertIcon, cbutton: string};
 
 @Component({
   selector: 'app-user-view',
@@ -11,9 +15,11 @@ import { UsersService } from '../../services/users.service';
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
-  user: IUser | null = null;
+  router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
   usersService = inject(UsersService)
+
+  user: IUser | null = null;
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(async (params: any) => {
@@ -21,4 +27,26 @@ export class UserViewComponent {
       this.user = await this.usersService.getByID(id);
     })
   }
+
+  async delete(_id: string | undefined) {
+    let borrado = confirm('Deseas Borrar el Usuario con ID: ' + _id + '?');
+    if (borrado) {
+      let alert_res: AlertResponse;
+      try {
+        const res = await this.usersService.deleteByID(_id);
+        if ('_id' in res && res._id === _id) {
+          alert_res = {title: 'Perfecto!', text: 'Usuario con ID: ' + _id + ' Borrado con exito', icon: 'success', cbutton: 'Aceptar'}
+        } else {
+          let text: string;
+          text = ('error' in res) ?  'Ha ocurrido un error' : 'Usuario con ID: ' + _id + ' No encontrado'
+          alert_res = {title: 'Error!', text: text, icon: 'error', cbutton: 'Aceptar'}
+        }
+      } catch (error) {
+        console.log(error);
+        alert_res = {title: 'Error!', text: 'Ha ocurrido un error', icon: 'error', cbutton: 'Aceptar'}
+      }
+      Swal.fire(alert_res)
+      this.router.navigate(['/dashboard', 'home'])
+    }
+  }  
 }
